3a766b8367f26641bd556b64156d57bcda19ab4f
nailuorenjian <1056017312@qq.com>
2024-10-16 21:21:11 +0900

    first commit

---
+++ src/main/java/com/example/springBatch/domain/BuUser.java
@@ -0,0 +1,12 @@
+package com.example.springBatch.domain;
+
+import lombok.Data;
+
+@Data
+public class BuUser {
+    private String userName;
+    private String sex;
+    private String age;
+    private String address;
+}
+
